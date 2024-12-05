886c8791afc3730c49eed8a6adf6fd6772f2f461
Josh Wilde <jowilde11@gmail.com>
2024-12-05 10:14:03 -0500

    complete challenge

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
-// index.js
+const renderRamen = (ramen) => {
+  const ramenMenu = document.querySelector('#ramen-menu');
+  const img = document.createElement('img');
+  img.src = ramen.image;
+  img.alt = ramen.name;
+  ramenMenu.appendChild(img);
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
+  img.addEventListener('click', () => {
+    const detailImage = document.querySelector('.detail-image');
+    const detailName = document.querySelector('.name');
+    const detailRestaurant = document.querySelector('.restaurant');
+    const detailsRating = document.querySelector('#rating-display');
+    const detailsComment = document.querySelector('#comment-display');
+
+    detailImage.src = ramen.image;
+    detailImage.alt = ramen.name;
+    detailName.textContent = ramen.name;
+    detailRestaurant.textContent = ramen.restaurant;
+    detailsRating.textContent = ramen.rating;
+    detailsComment.textContent = ramen.comment;
+  });
 };
 
 const addSubmitListener = () => {
-  // Add code
-}
+  const form = document.querySelector('#new-ramen');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newRamen = {
+      name: event.target.name.value,
+      restaurant: event.target.restaurant.value,
+      image: event.target.image.value,
+      rating: event.target.rating.value,
+      comment: event.target.comment.value,
+    };
+    renderRamen(newRamen);
+    form.reset();
+  });
+};
 
 const displayRamens = () => {
-  // Add code
+  fetch('http://localhost:8000/ramens')
+    .then(resp => resp.json())
+    .then(jsonData => {
+      jsonData.forEach(ramenObj => {
+        renderRamen(ramenObj);
+      });
+    });
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
-
-main()
-
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
+  displayRamens();
+  addSubmitListener();
 };
+
+document.addEventListener('DOMContentLoaded', main);
